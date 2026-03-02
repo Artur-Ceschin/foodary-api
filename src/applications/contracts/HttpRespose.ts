e0e1ad6658c6df06dc8862e4d1ce96502cd58775
Artur Ceschin <artur.ceschin@gmail.com>
2026-03-02 17:06:47 -0300

    feat: initial project structure and initial abstraction

---
+++ src/applications/contracts/HttpRespose.ts
@@ -0,0 +1,5 @@
+
+export interface IHttpResponse<TBody = undefined> {
+  statusCode: number,
+  body?: TBody
+}
---
+++ src/main/functions/hello.ts
@@ -0,0 +1,6 @@
+import { HelloController } from '../../applications/controllers/HelloController.js';
+import { lambdaHttpAdapter } from '../adapters/lambdaHttpAdapter.js';
+
+const controller = new HelloController();
+
+export const handler = lambdaHttpAdapter(controller);
